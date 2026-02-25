72a300e3e8d08cbc2642917e3405eae81ee240e6
SirSouza <sr.souza.contato@gmail.com>
2026-02-25 10:50:59 -0300

    Relógio analógico

---
+++ B7web-Projects/Relogio Analógico/script.js
@@ -0,0 +1,27 @@
+const digital = document.querySelector(".digital");
+const secPointer = document.querySelector(".p_s");
+const minPointer = document.querySelector(".p_m");
+const hourPointer = document.querySelector(".p_h");
+
+function updateClock() {
+  const now = new Date();
+
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  digital.textContent = `${fixZero(hour)}:${fixZero(minutes)}:${fixZero(seconds)}`;
+
+  let secDeg = (360 / 60) * seconds - 90;
+  let minDeg = (360 / 60) * minutes - 90;
+  let hourDeg = (360 / 12) * hour - 90;
+  secPointer.style.transform = `rotate(${secDeg}deg)`;
+  minPointer.style.transform = `rotate(${minDeg}deg)`;
+  hourPointer.style.transform = `rotate(${hourDeg}deg)`;
+}
+setInterval(updateClock, 1000);
+
+function fixZero(time) {
+  return time < 10 ? `0${time}` : time;
+}
+updateClock();
